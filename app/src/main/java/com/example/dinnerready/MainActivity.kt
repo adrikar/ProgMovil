5c469512c9998cf2e8f3f0b1746288266b3465b7
adrikar <orellanakaren30@gmail.com>
2020-10-10 17:14:13 -0400

    RegistroUsuario

--- app/src/main/java/com/example/dinnerready/MainActivity.kt
+++ app/src/main/java/com/example/dinnerready/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.dinnerready
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setup()
+    }
+
+    private fun setup(){
+        title= "Autenticacion"
+        singUpButton.setOnClickListener {
+            if(emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty() ){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener {
+                    if(it.isSuccessful){
+
+                    }else{
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog= builder.create()
+        dialog.show()
     }
 }
